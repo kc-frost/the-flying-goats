9536ea1285b3a1bf6eaf56e4ee5d809e8d552c2e
k128c <aeth.kr@gmail.com>
2026-02-19 16:20:18 -0600

    Add routing and basic routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { SignInUp } from './sign-in-up/sign-in-up';
+import { Home } from './home/home';
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: Home,
+    },
+    {
+        path: 'auth',
+        component: SignInUp,
+    },
+];
+
+export default routes;
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { provideRouter } from '@angular/router';
 
-bootstrapApplication(App, appConfig)
+import routes from './app/app.routes';
+
+bootstrapApplication(App, {
+  providers: [
+    provideRouter(routes),
+  ],
+})
   .catch((err) => console.error(err));
