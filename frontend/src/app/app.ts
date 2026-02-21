62bb2f6ae6aae001f2e6afd7c31c825295c75f58
k128c <aeth.kr@gmail.com>
2026-02-20 23:29:34 -0600

    Change border radius to dropdown box

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
+import { Login } from "./login/login";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, Login],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
