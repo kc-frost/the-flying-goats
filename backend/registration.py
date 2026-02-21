4425033a7ad88e4159689086407811b88af558b4
k128c <aeth.kr@gmail.com>
2026-02-21 10:47:13 -0600

    Upload root gitignore & implement validate_password()
    Also add comments to already implemented functions

--- backend/registration.py
+++ backend/registration.py
@@ -7,16 +7,13 @@ import hashlib
 import os
 from dotenv import load_dotenv
 import mysql.connector
-from mysql.connector.pooling import PooledMySQLConnection
-from mysql.connector.abstracts import MySQLConnectionAbstract
 
+def connect_to_db():
+    """Attempts to connect to the database using user variables from the
+    hidden .env file, and returns a connection object if successful
 
-def connect_to_db() -> PooledMySQLConnection | MySQLConnectionAbstract:
-    """
-    Attempts to connect to the database using user variables from the
-    hidden .env file, and returns a connection object if successful.    
-    :return: A object that's connected to the db and can do queries
-    :rtype: PooledMySQLConnection | MySQLConnectionAbstract
+    Returns:
+        _type_: A object that's connected to the db and can do queries
     """
 
     # database login credentials
@@ -36,24 +33,40 @@ def connect_to_db() -> PooledMySQLConnection | MySQLConnectionAbstract:
 # NOTE: This function only ever runs upon
 # validation of it being a good password
 def hash_password(password: str) -> str:
-    """
-    Using SHA224, returns a hashed password to be used
+    """Using SHA224, returns a hashed password to be used
     to insert into the database
-    
-    :param password: Unhashed password
-    :type password: str
-    :return: A hashed version of a validated password
-    :rtype: str
+
+    Args:
+        password (str): Unhashed password
+
+    Returns:
+        str: A hashed version of a validated password
     """
     encoded_pass = password.encode()
     hashed_pass = hashlib.sha224(encoded_pass).hexdigest()
 
     return hashed_pass
 
+# TODO (not immediate): Ensure that domains don't have hyphens as its
+# last character
 def validate_email(email: str) -> bool:
-    is_valid = False
-    
-    return is_valid
+    """Validates email according to specified regex pattern
+    Local Part (part before the @): 
+        Can have any alphanumeric characters and special characters
+    Domain (part after the @):
+        Can have any alphanumeric character
+
+    Args:
+        email (str): Inputted email of a user trying to authenticate 
+
+    Returns:
+        bool: If the email matches the pattern or not
+    """
+
+    VALID_PATTERN = r"[a-zA-Z0-9\.\*\+\?\$\^\/\\]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]+"
+    search = re.fullmatch(VALID_PATTERN, email.strip())
+
+    return search is not None
 
 def validate_password(password: str) -> bool:
     is_valid = False
