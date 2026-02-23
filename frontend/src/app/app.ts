2d8458749d0acb45fb7250ea7a5b61f66dd12939
rich93147 <richardwalker5357@gmail.com>
2026-02-23 00:40:06 -0600

    Added book-flight to app.ts imports

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { Login } from "./login/login";
 import { Inventory } from './inventory/inventory';
+import { BookFlight } from './book-flight/book-flight';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, Login, Inventory],
+  imports: [RouterOutlet, RouterLink, Login, Inventory, BookFlight],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
