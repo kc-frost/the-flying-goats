a292a24e9b8ac0a367a9ad427280fcbb0388b5c8
k128c <aeth.kr@gmail.com>
2026-02-26 15:01:35 -0600

    Add comments for how to setup a Blueprint

--- backend/src/main.py
+++ backend/src/main.py
@@ -14,12 +14,12 @@ cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 # This is to not clog this file, and be able to divide responsibilities across multiple files.
 # By also dividing work like this, it becomes easier to work independently, as we become prone to merge conflicts otherwise.
 
-"""
-url_prefix does what you think it does.
-In order to access any of the routes present in authbp for example, the client has to make sure their links are prepended with [url_prefix]
-Ex. "localhost:5000/login" will return a 404
-Meanwhile, "localhost:5000/api/login" can potentially return a response
-"""
+# What is url_prefix?
+# url_prefix does what you think it does
+# In order to access any of the routes present in a Blueprint, the client has to make sure their links are prepended with the Blueprint's respective[url_prefix]
+# Ex. "localhost:5000/login" will return a 404
+# Meanwhile, "localhost:5000/api/login" can potentially return a response
+
 app.register_blueprint(authbp, url_prefix="/api")
 
 if __name__ == "__main__":
