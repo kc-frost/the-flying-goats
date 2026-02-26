3d1c751277dea6b89e775b0290369cee6e457cf2
k128c <aeth.kr@gmail.com>
2026-02-25 23:07:58 -0600

    Add CORS and enable conn to db

--- backend/src/auth/routes.py
+++ backend/src/auth/routes.py
@@ -1,12 +1,34 @@
-from flask import Flask, jsonify, request, Blueprint
-from main import app
+from flask import Flask, jsonify, request, Blueprint, session
+from flask_cors import CORS
+from db import get_connection
 
-bp = Blueprint("auth", __name__, url_prefix="/api")
+bp = Blueprint("auth", __name__)
+CORS(bp)
 
-@bp.route('/login')
+@bp.post('/login')
+def login():    
+    data = request.json
+    email = data['email']
+    password = data['password']
+
+    conn = get_connection()
+    with conn.cursor() as cursor:
+        sql = "SELECT `email`, `password` FROM `users` WHERE `email`=%s AND `password`=%s"
+        cursor.execute(sql, (email, password))
+        result = cursor.fetchone()
+
+    if result is not None:
+        return jsonify(200)
+    else:
+        return jsonify(404)
+
+@bp.route('/register')
 def register():
+    conn = get_connection()
+    with conn.cursor() as cursor:
+        sql = "SELECT * FROM `users`"
+        cursor.execute(sql)
+        result = cursor.fetchone()
+
     return f"asd"
 
-@app.route('/api/login')
-def login():
-    return f"asd"
--- backend/src/main.py
+++ backend/src/main.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
+from auth.routes import bp
 
 app = Flask(__name__)
-
-from routes import auth
+CORS(app)
+app.register_blueprint(bp, url_prefix="/api")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
