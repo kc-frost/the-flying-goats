5ca44baeacb787b7e841865a65dfdfca26a45205
k128c <aeth.kr@gmail.com>
2026-03-02 15:51:23 -0600

    Add function to check if session exists
    Implies an authenticated user exists

--- frontend/src/app/_shared/services/auth-service.ts
+++ frontend/src/app/_shared/services/auth-service.ts
@@ -1,10 +1,21 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { environment } from '../../../_environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   private isLoggedIn: boolean = false;
+  http = inject(HttpClient);
+  
+  // returns blueprint for the request
+  // does not actually send it yet
+  checkSession() {
+    return this.http.get(`${environment.api_url}/api/check-session`,
+      {observe: 'response', withCredentials: true}
+    );
+  }
 
   getLoggedIn(): boolean {
     return this.isLoggedIn;
