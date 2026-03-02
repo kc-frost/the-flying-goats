1e4f319b9019d6b77fdf784c35f31c49473bd766
k128c <aeth.kr@gmail.com>
2026-03-01 21:40:33 -0600

    Add profile page and its dropdown ver

---
+++ frontend/src/app/profile-page/profile-dropdown/profile-dropdown.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-dropdown',
+  imports: [],
+  templateUrl: './profile-dropdown.html',
+  styleUrl: './profile-dropdown.css',
+})
+export class ProfileDropdown {
+
+}
