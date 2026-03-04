29ea4886f29e6ae1b384ad386f6fed70993c44dc
k128c <aeth.kr@gmail.com>
2026-03-04 13:14:28 -0600

    Finish what user info block looks like

--- frontend/src/app/profile-page/profile-page/profile-page.ts
+++ frontend/src/app/profile-page/profile-page/profile-page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { UserService } from '../../_shared/services/user-service';
 
 @Component({
   selector: 'app-profile-page',
@@ -7,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrl: './profile-page.css',
 })
 export class ProfilePage {
+  private userService = inject(UserService);
+  staticProfileImage = "/profile/static-profile-image.svg";
+  username: string = "randomusernameasdasdasasd"
+  email: string = "testEmail"
+
+
+  // loadUser() {
+  //   this.userService.loadUserDetails().subscribe({
+  //     next: (res) => {
+  //       var data = JSON.parse(JSON.stringify(res.body))
+
+  //       this.userService.setUsername(data['username'])
+  //       this.userService.setEmail(data['email'])
+
+  //       this.username = this.userService.getUsername();
+  //       this.email = this.userService.getEmail();
+  //     }
+  //   })
+  // }
 
 }
