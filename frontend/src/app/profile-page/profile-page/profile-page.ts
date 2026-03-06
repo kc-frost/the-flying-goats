e5a64ceade51f53f1bdec4af2c9e20e05ca9d30f
k128c <aeth.kr@gmail.com>
2026-03-05 21:04:44 -1200

    Begin add dyanmic rendering to profile view

--- frontend/src/app/profile-page/profile-page/profile-page.ts
+++ frontend/src/app/profile-page/profile-page/profile-page.ts
@@ -1,5 +1,21 @@
 import { Component, inject } from '@angular/core';
 import { UserService } from '../../_shared/services/user-service';
+import { form } from '@angular/forms/signals';
+
+interface Reservation {
+  id: number;
+  reservationDate: string;
+  flightID: string;
+  departureDate: string;
+  arrivalDate: string;
+  username: string;
+  seatNumber: string;
+  seatClass: string;
+  origin: string;
+  destination: string;
+
+
+}
 
 @Component({
   selector: 'app-profile-page',
@@ -10,4 +26,79 @@ import { UserService } from '../../_shared/services/user-service';
 export class ProfilePage {
   userService = inject(UserService);
   staticProfileImage = "/profile/static-profile-image.svg";
+
+  formatDate(date: string): string {
+    var enteredDate = new Date(date);
+    var formattedDate = new Intl.DateTimeFormat("en-GB", 
+      {
+        weekday: "short",
+        day: "2-digit",
+        month: "short",
+        year: "numeric",
+      }).format(enteredDate);
+
+      return formattedDate;
+  }
+
+  formatTime(date: string) {
+    var enteredDate = new Date(date);
+    var hour12Time = new Intl.DateTimeFormat("en-GB",
+      {
+        hour12: true,
+        hour: "2-digit",
+        minute: "numeric"
+      }
+    ).format(enteredDate)
+    hour12Time = hour12Time.replace(" ", "");
+    
+    var hour24Time = new Intl.DateTimeFormat("en-GB",
+      {
+        hour12: false,
+        hour: "2-digit",
+        minute: "numeric"
+      }
+    ).format(enteredDate)
+
+    return hour12Time.concat(" / " + hour24Time);
+  }
+  
+  testReservation: Reservation = {
+    id: 1,
+    arrivalDate: "2026-03-07T20:25",
+    departureDate: "2026-03-06T20:25",
+    destination: "Paris",
+    flightID: "PRS321",
+    origin: "Austin",
+    reservationDate: "Thu Mar 05 2026",
+    seatClass: "Economy", 
+    seatNumber: "",
+    username: "admin"
+  }
+
+  arrayOfReservations: Reservation[] = [
+    {
+      id: 1,
+      arrivalDate: "2026-03-07T20:25",
+      departureDate: "2026-03-06T20:25",
+      destination: "Paris",
+      flightID: "PRS321",
+      origin: "Austin",
+      reservationDate: "Thu Mar 05 2026",
+      seatClass: "Economy", 
+      seatNumber: "",
+      username: "admin"
+    },
+    {
+      id: 2,
+      arrivalDate: "2026-03-27T00:02",
+      departureDate: "2026-03-23T20:58",
+      destination: "Japan",
+      flightID: "JPN987",
+      origin: "Austin",
+      reservationDate: "Thu Mar 05 2026",
+      seatClass: "Economy",
+      seatNumber: "",
+      username: "admin"
+    }
+  ];
 }
