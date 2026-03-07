a22a8829bfaf67a01a3aa0a451e571704d72a096
k128c <aeth.kr@gmail.com>
2026-03-06 22:53:02 -0600

    preemtpively add guards in prep of richard merge

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -38,6 +38,16 @@ export const routes: Routes = [
         component: ProfilePage,
         canActivate: [userAuthGuard],
     },
+    {
+        path: 'view-appointments',
+        component: ViewAppointments,
+        canActivate: [userAuthGuard, adminAuthGuard],
+    },
+    {
+        path: 'view-users',
+        component: ViewUsers,
+        canActivate: [userAuthGuard, adminAuthGuard],
+    },
     // dropdown outlet
     {
         path: 'login',
@@ -62,6 +72,7 @@ export const routes: Routes = [
         component: Inventory,
         canActivate: [userAuthGuard, adminAuthGuard],
     },
+    
 ];
 
 export default routes;
