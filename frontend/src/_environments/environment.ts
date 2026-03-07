bb0787d77fd4471567f0d3312f87a04c99d0c9be
rich93147 <richardwalker5357@gmail.com>
2026-03-06 18:47:40 -0600

    changed baseurl to ip

--- frontend/src/_environments/environment.ts
+++ frontend/src/_environments/environment.ts
@@ -1,3 +1,5 @@
 // DEVELOPMENT
 
-export const BASE_URL = "http://localhost:5000";
+// change this back to localhost if testing locally
+// export const BASE_URL = "http://localhost:5000";
+export const BASE_URL = "http://159.65.100.137:5000";
