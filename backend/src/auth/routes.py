0717dc3977be3096932a46b4a7f5e977e83f2b2d
Rich93147 <richardwalker5357@gmail.com>
2026-03-06 23:39:53 -0600

    Fixed service imports

--- backend/src/auth/routes.py
+++ backend/src/auth/routes.py
@@ -1,8 +1,7 @@
 from flask import jsonify, request, Blueprint
 from flask_login import login_user, login_required, current_user, logout_user
 from db import get_connection
-from .service import delete_from_inventory, find_user, get_reservations, insert_into_inventory, insert_user, find_inventory, check_ifadmin, book_a_flight
-from .service import delete_from_inventory, find_user, get_reservations, get_user_data, insert_into_inventory, insert_user, find_inventory, update_inventory
+from .service import delete_from_inventory, find_user, get_reservations, get_user_data, insert_into_inventory, insert_user, find_inventory, update_inventory, check_ifadmin, book_a_flight
 from .validators import validate_email, validate_password
 from .security import admin_required
 from _models.user import User
