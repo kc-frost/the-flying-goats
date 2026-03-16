9351322023b43fa07caf3c49abfb8575d3916737
k128c <aeth.kr@gmail.com>
2026-03-15 21:06:37 -0500

    Create new seat-select modal

---
+++ frontend/src/app/book-flight/seat-selector-modal/seat-selector-modal.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seat-selector-modal',
+  imports: [],
+  templateUrl: './seat-selector-modal.html',
+  styleUrl: './seat-selector-modal.css',
+})
+export class SeatSelectorModal {
+
+}
