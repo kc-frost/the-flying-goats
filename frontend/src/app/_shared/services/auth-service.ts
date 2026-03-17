8d7021f673078453f12f12c0320bcd4c423586a6
k128c <aeth.kr@gmail.com>
2026-03-17 00:06:44 -0500

    Properly use Observables instead of LocalStorage

--- frontend/src/app/_shared/services/auth-service.ts
+++ frontend/src/app/_shared/services/auth-service.ts
@@ -1,62 +1,81 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../../_environments/environment';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
+import { UserService } from './user-service';
+import { FormGroup } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  // ! == asserts that this property cannot be not null or undefined
-  // (because it won't be, this will be instantiated in app.ts, but it doesn't know that)
-  private authenticated!: boolean
-  private admin: boolean = false
+  constructor(private http: HttpClient, private userService: UserService) {}
 
-  constructor(private http: HttpClient) {}
-
-  setAdminTrue() { localStorage.setItem('isAdmin', 'true'); }
-  setAdminFalse() { localStorage.setItem('isAdmin', 'false')};
-  isAdmin() { return localStorage.getItem('isAdmin') === 'true'; }
-
-  setAuthenticatedTrue() { localStorage.setItem('authenticated', 'true'); }
-  setAuthenticatedFalse() { localStorage.setItem('authenticated', 'false'); }
-  isAuthenticated() { return localStorage.getItem('authenticated') === 'true'; }
- 
-  // Expected JSON response:
-  // {"authenticated": true/false,
-  //  "isAdmin": true/false,
-  //  "username": current_user.username | "null"}
-  getUserSummary() {
-    return this.http.get(`${environment.api_url}/api/check-session`,
-      {withCredentials: true}
-    );
+  // Holds current value and notifies anyone who .subscribes() to it if its value changes
+  // Defaults to false for both
+  private authenticated = new BehaviorSubject<boolean>(false);
+  private adminStatus = new BehaviorSubject<boolean>(false);
+  
+  // Exposes the 'current' values of currentEmail and currentUsername
+  // Subscribe to these (in a component) to automatically react when the value changes via async pipe
+  authenticated$ = this.authenticated.asObservable();
+  adminStatus$ = this.adminStatus.asObservable();
+  
+  setAuthenticatedTrue(): void {
+    this.authenticated.next(true);
   }
 
-  login(userProfile: any) {
-    const _headers = {
-      'Content-Type': 'application/json'
-    };
+  // Use for one-time synchronous reads
+  getAuthenticated(): boolean {
+    return this.authenticated.getValue();
+  }
+
+  setAdminTrue(): void {
+    this.adminStatus.next(true);
+  }
+
+  // Use for one-time synchronous reads
+  getAdminStatus(): boolean {
+    return this.adminStatus.getValue();
+  }
+
+  reset(): void {
+    this.authenticated.next(false);
+    this.adminStatus.next(false);
+  }
+
+  login(userProfile: FormGroup): Observable<HttpResponse<any>> {
+    return this.http.post(`${environment.api_url}/api/login`, 
+      userProfile.value, 
+      { observe: 'response'}
     
-    return this.http.post(`${environment.api_url}/api/login`,
-      userProfile,
-      { headers: _headers, observe: 'response'}
-    );
+      // Before being subscribed to (.subscribe()), set the currentUser's email
+    ).pipe(
+      tap((res: any) => {
+        this.userService.setEmail(userProfile.get('email')!.value);
+        this.userService.setUsername(res.body.username);
+
+        this.setAuthenticatedTrue();
+        if (res.body.is_admin) {
+          this.setAdminTrue()
+        }
+      })
+    )
   }
   
-  register(newUserProfile: any) {
+  register(newUserProfile: FormGroup): Observable<HttpResponse<any>> {
     const _headers = {
       'Content-Type': 'application/json'
     };
 
-    return this.http.post(`${environment.api_url}/api/register`, newUserProfile,
+    return this.http.post(`${environment.api_url}/api/register`, newUserProfile.value,
       { headers: _headers, observe: 'response'}
     )
   }
 
-  logout() {
-    // can only do this here because login doesn't know whether it pushes through or not yet
-    // here, we already know the user is logged in
-    this.setAuthenticatedFalse()
-    // this.setAdminFalse()
+  logout(): Observable<HttpResponse<any>> {
+    this.reset();
+
     return this.http.get(`${environment.api_url}/api/logout`,
       { observe: 'response'}
     )
