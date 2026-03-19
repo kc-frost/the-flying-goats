974d348c40f3c69be8419399829660614545ffa9
k128c <aeth.kr@gmail.com>
2026-03-18 20:14:17 -0500

    feat(seat-selector): add submit and formgroup
    now able to emit user's selected seat to the book-flight component

--- frontend/src/app/book-flight/seat-selector-modal/seat-selector-modal.ts
+++ frontend/src/app/book-flight/seat-selector-modal/seat-selector-modal.ts
@@ -1,20 +1,31 @@
-import { Component, output } from '@angular/core';
+import { Component, output, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-seat-selector-modal',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './seat-selector-modal.html',
   styleUrl: './seat-selector-modal.css',
 })
 export class SeatSelectorModal {
-  exitModal = output<boolean>();
+  private formBuider = inject(FormBuilder);
 
+  exitModal = output<boolean>();
   tellExit(): void {
     this.exitModal.emit(false);
   }
 
+  seatID = output<string>();
+  mySeat = this.formBuider.group({
+    seatID: [''],
+  });
+
+  onSubmit() {
+    this.seatID.emit(this.mySeat.get('seatID')?.value!);
+  }
+
   // HARCODED SEAT IDs
-  rows = [1, 2, 3, 4];
+  rows = [1, 2, 3, 4, 5, 6, 7];
   leftSeats = ["A", "B"];
   rightSeats = ["C", "D"];
   seats = ["A", "B", "C", "D"];
