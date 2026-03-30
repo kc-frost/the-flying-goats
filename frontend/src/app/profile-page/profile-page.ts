654ed72db247724180ed9df37d15fd91c9f3ab19
k128c <aeth.kr@gmail.com>
2026-03-29 23:49:24 -0500

    refactor: sort reservations based on date
    combined 'current' and 'future' to 'upcoming'

--- frontend/src/app/profile-page/profile-page.ts
+++ frontend/src/app/profile-page/profile-page.ts
@@ -1,9 +1,11 @@
-import { ChangeDetectorRef, Component, inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { AsyncPipe } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { UserService } from '../_shared/services/user-service';
 import { environment } from '../../_environments/environment';
+import { BehaviorSubject } from 'rxjs';
+import { DatePipe } from '@angular/common';
 
 interface Reservation {
   bookingNumber: number;
@@ -21,139 +23,83 @@ interface Reservation {
 @Component({
   selector: 'app-profile-page',
   // AsyncPipe subscribes to an Observable/Promise and emits the latest value emitted, and then is marked to be checked for changes
-  imports: [ReactiveFormsModule, AsyncPipe],
+  imports: [ReactiveFormsModule, AsyncPipe, DatePipe],
   templateUrl: './profile-page.html',
   styleUrl: './profile-page.css',
 })
 export class ProfilePage {
-  private cdr = inject(ChangeDetectorRef);
+  private http = inject(HttpClient);
+  private userReservations = new BehaviorSubject<any[]>([]);
   userService = inject(UserService);
+  selectedTab: 'past' | 'upcoming' = 'upcoming'
   staticProfileImage = "/profile/static-profile-image.svg";
 
-  today = new Date();
-  selectedTab: 'past' | 'current' | 'future' = 'past'
-  userReservations: Reservation[] = [];
-
-  // sorts RESERVATION DATE in descending order
-  constructor(private http: HttpClient) {
+  ngOnInit() {
     this.loadReservations();
-    this.loadBio();
-
   }
 
   loadReservations() {
-    this.http.get<Reservation[]>(`${environment.api_url}/api/get-user-reservations`, {withCredentials: true})
-    .subscribe({
+    this.http.get<any[]>(`${environment.api_url}/api/get-user-reservations`).subscribe({
       next: (data) => {
-        this.userReservations = data;
-        this.userReservations.sort((a, b) =>
-          new Date(b.reservationDate).getTime() - new Date(a.reservationDate).getTime()
-        );
+        this.userReservations.next(data);
       },
       error: (err) => {
-        console.log(err);
+        console.log("LOADING RESERVATIONS ERROR:", err);
       }
     })
   }
 
-  // assumption: all returned reservations from the database will automatically be made by the current logged in user
-  // main function to return reservations
-  getReservations() {
-    if (this.selectedTab === 'past') return this.getPastReservations();
-    if (this.selectedTab === 'current') return this.getCurrentReservations();
-    return this.getFutureReservations();
-  }
+  get filteredReservations() {
+    const today = new Date();
+    const allReservations = this.userReservations.value;
 
-  // the three succeeding functions get reservations based on time relative to today 
+    // past: descending (most recent first)
+    if (this.selectedTab == 'past') {
+      return allReservations.filter((reservation) => new Date(reservation.reservationDate) < today)
+      .sort((a,b) => 
+        new Date(b.reservationDate).getTime() - new Date(a.reservationDate).getTime()
+      )
+    } 
 
-  getPastReservations() {
-    return this.userReservations
-    .filter(r => new Date(r.liftOffDate) < this.today)
-    .sort((a, b) => new Date(a.liftOffDate).getTime() - new Date(b.liftOffDate).getTime());
-  }
-  
-  getCurrentReservations() {
-    return this.userReservations
-    .filter(r => {
-      const dep = new Date(r.liftOffDate);
-      const arr = new Date(r.arrivingDate);
-      return dep <= this.today && arr >= this.today;
-    })
-    .sort((a, b) => new Date(a.liftOffDate).getTime() - new Date(b.liftOffDate).getTime());
-  }
-  
-  getFutureReservations() {
-    return this.userReservations
-    .filter(r => new Date(r.liftOffDate) > this.today)
-    .sort((a, b) => new Date(a.liftOffDate).getTime() - new Date(b.liftOffDate).getTime());
-  }
-
-  // formatting of dates and time
-  formatDate(date: string): string {
-    var enteredDate = new Date(date);
-    var formattedDate = new Intl.DateTimeFormat("en-GB", 
-      {
-        weekday: "short",
-        day: "2-digit",
-        month: "short",
-        year: "numeric",
-      }).format(enteredDate);
-
-      return formattedDate;
-  }
-
-  formatTime(date: string) {
-    var enteredDate = new Date(date);
-    var hour12Time = new Intl.DateTimeFormat("en-GB",
-      {
-        hour12: true,
-        hour: "2-digit",
-        minute: "numeric"
-      }
-    ).format(enteredDate)
-    hour12Time = hour12Time.replace(" ", "");
-    
-    var hour24Time = new Intl.DateTimeFormat("en-GB",
-      {
-        hour12: false,
-        hour: "2-digit",
-        minute: "numeric"
-      }
-    ).format(enteredDate)
-
-    return hour12Time.concat(" / " + hour24Time);
+    // upcoming: ascending (soonest first)
+    else {
+      return allReservations.filter((reservation) => new Date(reservation.reservationDate) >= today)
+      .sort((a,b) => 
+        new Date(a.reservationDate).getTime() - new Date(b.reservationDate).getTime()
+      )
+    }
   }
 
   // bio logic
-  isEditing = false;
-  userBio = new FormControl('');
+//   isEditing = false;
+//   userBio = new FormControl('');
 
-loadBio() {
-  this.http.get<{bio: string}>(`${environment.api_url}/api/get-bio`, { withCredentials: true })
-  .subscribe({
-    next: (data) => {
-      // console.log('bio response:', data);
-      this.userBio.setValue(data.bio ?? '');
-      this.cdr.detectChanges();
-    },
-    error: (err) => console.log(err)
-  });
-}
+// loadBio() {
+//   this.http.get<{bio: string}>(`${environment.api_url}/api/get-bio`, { withCredentials: true })
+//   .subscribe({
+//     next: (data) => {
+//       // console.log('bio response:', data);
+//       this.userBio.setValue(data.bio ?? '');
+//       this.cdr.detectChanges();
+//     },
+//     error: (err) => console.log(err)
+//   });
+// }
 
-saveBio() {
-  this.http.post(
-    `${environment.api_url}/api/save-bio`, 
-    { bio: this.userBio.value }, 
-    { withCredentials: true }
-  ).subscribe({
-    next: () => this.isEditing = false,
-    error: (err) => console.log(err)
-  });
-}
+// saveBio() {
+//   this.http.post(
+//     `${environment.api_url}/api/save-bio`, 
+//     { bio: this.userBio.value }, 
+//     { withCredentials: true }
+//   ).subscribe({
+//     next: () => this.isEditing = false,
+//     error: (err) => console.log(err)
+//   });
+// }
 
-updateBio() {
-  this.isEditing = true;
-}
+// updateBio() {
+//   this.isEditing = true;
+// }
 
   // saveBio() {
   //   this.userBio.setValue(this.userBio.value);
