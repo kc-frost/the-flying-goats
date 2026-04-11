3afd2501a2c46c213951583d4cc10e38ccf2ed65
k128c <aeth.kr@gmail.com>
2026-04-10 23:17:25 -0500

    style: design analytics page, no logic yet
    thats why its tagged as style

--- frontend/src/app/admin/analytics/analytics.ts
+++ frontend/src/app/admin/analytics/analytics.ts
@@ -7,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrl: './analytics.css',
 })
 export class Analytics {
+  // return this format in the api
 
+  top3Users = [
+    {user: 'me', amt: 123},
+    {user: 'you', amt: 21},
+    {user: 'someone', amt: 12},
+  ];
+
+  longestRegisteredUsers = [
+    {user: 'me', days: 123},
+    {user: 'you', days: 23},
+    {user: 'someone', days: 9},
+  ];
+
+  reservationsThisYear = [
+    {month: 'Jan', amt: 12},
+    {month: 'Feb', amt: 12},
+    {month: 'Mar', amt: 12},
+    {month: 'Apr', amt: 12},
+    {month: 'May', amt: 12},
+    {month: 'Jun', amt: 12},
+    {month: 'July', amt: 12},
+    {month: 'Aug', amt: 12},
+    {month: 'Sep', amt: 12},
+    {month: 'Oct', amt: 12},
+    {month: 'Nov', amt: 12},
+    {month: 'Dec', amt: 12},
+  ];
 }
