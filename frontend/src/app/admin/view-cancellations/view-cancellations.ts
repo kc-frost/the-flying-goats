9483c0c6f39c7132bc5819195517757879bedcaf
k128c <aeth.kr@gmail.com>
2026-04-13 17:59:05 -0500

    feat: add view-cancellations component

---
+++ frontend/src/app/admin/view-cancellations/view-cancellations.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-cancellations',
+  imports: [],
+  templateUrl: './view-cancellations.html',
+  styleUrl: './view-cancellations.css',
+})
+export class ViewCancellations {
+
+}
