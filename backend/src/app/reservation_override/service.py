db4f44d0675441b885226e06f67d31ab36d22ae4
k128c <aeth.kr@gmail.com>
2026-04-13 16:45:53 -0500

    feat: update cancelled bookings through backend
    AAAAAAAAAAA

--- backend/src/app/reservation_override/service.py
+++ backend/src/app/reservation_override/service.py
@@ -19,16 +19,12 @@ def get_cancelleable_reservations():
                         ad.IATA as destination
                     FROM
                         bookinghistory bh
-                            INNER JOIN
-                        schedule sd ON bh.departScheduleID = sd.scheduleID
-                            INNER JOIN
-                        flight fd ON sd.flightID = fd.IATA
-                            INNER JOIN
-                        airports ao ON fd.origin = ao.airportID
-                            INNER JOIN
-                        airports ad ON fd.destination = ad.airportID
-                            INNER JOIN
-                        users u ON bh.userID = u.userID 
+                    INNER JOIN schedule sd ON bh.departScheduleID = sd.scheduleID
+                    INNER JOIN flight fd ON sd.flightID = fd.IATA
+                    INNER JOIN airports ao ON fd.origin = ao.airportID
+                    INNER JOIN airports ad ON fd.destination = ad.airportID
+                    INNER JOIN users u ON bh.userID = u.userID 
+                    WHERE bh.bookingStatus != 'Cancelled'
                 """
 
                 cursor.execute(query,)
@@ -71,7 +67,8 @@ def get_cancelleable_reservations():
                         INNER JOIN flight fd ON sd.flightID = fd.IATA
                         INNER JOIN airports ao ON fd.origin = ao.airportID
                         INNER JOIN airports ad ON fd.destination = ad.airportID
-                        INNER JOIN users u ON bh.userID = u.userID) af ON pf.IATA = af.IATA
+                        INNER JOIN users u ON bh.userID = u.userID
+                        WHERE bh.bookingStatus != 'Cancelled') af ON pf.IATA = af.IATA
                     WHERE
                         pf.email = %s;
                 """
@@ -89,3 +86,33 @@ def get_cancelleable_reservations():
         
         else:
             return {'err': 'you are neither an admin or a pilot. get out of here'}
+        
+def do_override_reservation(bookingNumber: int, reason: str, staffID: int | None):
+    conn = get_connection()
+
+    with conn.cursor() as cursor:
+        try:
+            query = """
+                DELETE FROM `booking`
+                WHERE `bookingNumber` = %s
+            """
+
+            cursor.execute(query, (bookingNumber,))
+
+            query = """
+                UPDATE `bookinghistory`
+                SET `cancelledBy` = %s, `reason` = %s
+                WHERE `bookingNumber` = %s
+            """
+
+            # TODO: Admin is a role now, adjust for that later
+            if (current_user.is_admin):
+                staffID = current_user.id
+            
+            cursor.execute(query, (staffID, reason, bookingNumber, ))
+
+            conn.commit()
+
+        except Exception as e:
+            conn.rollback()
+            return {'err': str(e)}
