66b54575ae9f8e4ae3b213539ad9b872bc5de4c5
k128c <aeth.kr@gmail.com>
2026-04-13 11:10:50 -0500

    feat: generate service for reservation-override

---
+++ frontend/src/app/admin/reservation-override/utils/override-service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { environment } from '../../../../_environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class OverrideService {
+  private http = inject(HttpClient);
+
+  // ADMIN gets these fields
+  // flight IATA
+  // bookers username
+  // liftOff of both departing and return flights
+  // landing of both departing and return flights
+  // origin airport IATA of ""
+  // destination airport IATA of "" 
+
+  // PILOT gets these fields
+  // staffID of pilot
+  // email of pilot
+  // ...the rest of the admin fields
+  getCancelleableReservations() {
+    return this.http.get(`${environment.api_url}/admin/cancelleable-reservations`);
+  }
+}
