3c8155de8975b3db35cba21789551d1c80b1c739
k128c <aeth.kr@gmail.com>
2026-04-13 21:15:57 -0500

    feat: dynamically show all reservations
    this includes notable details like whose reservation, when, where,
    and details about the cancellation itself

--- frontend/src/app/admin/view-cancellations/view-cancellations.ts
+++ frontend/src/app/admin/view-cancellations/view-cancellations.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { CancellationService } from './utils/cancellation-service';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-view-cancellations',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './view-cancellations.html',
   styleUrl: './view-cancellations.css',
 })
 export class ViewCancellations {
+  private cancellation = inject(CancellationService);
+
+  // BEHAVIORSUBJEEEEECT
+  private allCancellations = new BehaviorSubject<{ bookingNumber: number, bookingDate: string, cancellationDate: string, cancelledBy: string, destination: string, origin: string, reason: string, username: string }[]>([]);
+  allCancellations$ = this.allCancellations.asObservable();
+
+  ngOnInit() {
+    this.getAllCancellations();
+  }
+
+  getAllCancellations() {
+    this.cancellation.getAllCancellations().subscribe({
+      next: (res) => {
+        // console.log(res);
+        this.allCancellations.next(res.map(
+          r => ({
+            bookingNumber: r.bookingNumber,
+            bookingDate: r.bookingDate,
+            cancellationDate: r.cancellationDate,
+            cancelledBy: r.cancelledBy,
+            destination: r.destination,
+            origin: r.origin,
+            reason: r.reason,
+            username: r.username
+          })
+        ))
+      }
+    })
+  }
 
 }
